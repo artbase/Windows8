6f7359dda7e0223c31f1a7d441d3ccfcc943e368
artbase <bartosz.zaremba@live.com>
2013-06-01 18:12:40 +0200

    Czytnik kanału na Twitter

---
+++ TwitterTemplata/TwitterTemplata/App.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System.Reflection;
+using System;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using StyleMVVM;
+using Windows.UI.ApplicationSettings;
+using Callisto.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace TwitterTemplata
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                if (!rootFrame.Navigate(typeof(SplitPage), args.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+            SettingsPane.GetForCurrentView().CommandsRequested += OnCommandsRequested;
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private async void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+
+
+        void OnCommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            var privacyPolicy = new SettingsCommand("privacyPolicy", "Polityka prywatności", (handler) =>
+            {
+                var settings = new SettingsFlyout(); settings.Content = new PrivacyPolicy();
+                settings.HeaderBrush = new SolidColorBrush(Color.FromArgb(255, 10, 194, 255));
+                settings.Background = new SolidColorBrush(Color.FromArgb(255, 10, 194, 255));
+                settings.HeaderText = "Polityka prywatności";
+                settings.IsOpen = true;
+            });
+            args.Request.ApplicationCommands.Add(privacyPolicy);
+        }
+    }
+}
